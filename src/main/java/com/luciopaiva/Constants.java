6c81e0e443e4154770d90dbd233eb9bf0095d4db
Lucio Paiva <luciopaiva@gmail.com>
2020-01-24 01:02:17 +0000

    Increase server packet sizes to 1kB

--- src/main/java/com/luciopaiva/Constants.java
+++ src/main/java/com/luciopaiva/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     static final int SELECT_TIMEOUT = 100;
     static final int SERVER_PORT = 3023;
     /** must be a power of two and greater than a long */
-    static final int PACKET_SIZE_IN_BYTES = 128;
+    static final int PACKET_SIZE_IN_BYTES = 1024;
 }
