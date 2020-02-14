80a2873de009fb10b883a37d5731a6adae9e3ff6
Lucio Paiva <luciopaiva@gmail.com>
2020-02-14 20:41:26 +0000

    Add more resolution to bytesToStr()

--- src/main/java/com/luciopaiva/Utils.java
+++ src/main/java/com/luciopaiva/Utils.java
@@ -6,7 +6,9 @@ import java.net.SocketAddress;
 class Utils {
 
     private final static long KILO = 1024;
+    private final static long HUNDRED_KILO = 100 * KILO;
     private final static long MEGA = KILO * KILO;
+    private final static long HUNDRED_MEGA = 100 * MEGA;
     private final static long GIGA = KILO * MEGA;
 
     static String getAddressStr(SocketAddress socketAddress) {
@@ -15,9 +17,9 @@ class Utils {
     }
 
     static String bytesToStr(long bytes) {
-        if (bytes < KILO) {
+        if (bytes < HUNDRED_KILO) {
             return String.valueOf(bytes);
-        } else if (bytes < MEGA) {
+        } else if (bytes < HUNDRED_MEGA) {
             return (bytes / KILO) + "k";
         } else {
             return (bytes / MEGA) + "M";
